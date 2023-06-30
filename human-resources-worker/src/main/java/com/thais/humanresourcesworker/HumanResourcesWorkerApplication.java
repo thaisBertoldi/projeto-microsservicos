9a1dd6d8840ec8cdffa26b3f517cef90ef6e2620
thaisBertoldi <thais_brt@hotmail.com>
2023-06-29 21:39:57 -0300

    init: Projeto iniciado

---
+++ human-resources-worker/src/main/java/com/thais/humanresourcesworker/HumanResourcesWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.thais.humanresourcesworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesWorkerApplication.class, args);
+	}
+
+}
---
+++ human-resources-worker/src/test/java/com/thais/humanresourcesworker/HumanResourcesWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thais.humanresourcesworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
