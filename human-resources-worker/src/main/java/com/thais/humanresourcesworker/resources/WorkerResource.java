e2963af48c9b92c42a00a38d6173792f8fe1e684
thaisBertoldi <thais_brt@hotmail.com>
2023-07-03 20:04:07 -0300

    feat: inclusao de método findById do Worker

--- human-resources-worker/src/main/java/com/thais/humanresourcesworker/resources/WorkerResource.java
+++ human-resources-worker/src/main/java/com/thais/humanresourcesworker/resources/WorkerResource.java
@@ -4,6 +4,8 @@ import com.thais.humanresourcesworker.entities.Worker;
 import com.thais.humanresourcesworker.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +18,15 @@ public class WorkerResource {
     @Autowired
     private WorkerRepository workerRepository;
 
+    @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = workerRepository.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        Worker worker = workerRepository.findById(id).get();
+        return ResponseEntity.ok(worker);
     }
 }
