81352efc8af57fe2e93be41ed3ac6de069549033
thaisBertoldi <thais_brt@hotmail.com>
2023-07-05 18:47:01 -0300

    feat: incluindo biblioteca lombok e criada classe Payment

---
+++ human-resources-payroll/src/main/java/com/thais/hrpayroll/entities/Payment.java
@@ -0,0 +1,24 @@
+package com.thais.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Payment implements Serializable {
+
+    private String name;
+    private Double dailyIncome;
+    private Integer days;
+
+    public double getTotal() {
+        return days * dailyIncome;
+    }
+
+}
