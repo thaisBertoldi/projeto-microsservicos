32a638da2aa7dac7c4e2d6c246fafbda1544da43
thaisBertoldi <thais_brt@hotmail.com>
2023-07-07 08:03:06 -0300

    feat: inclusão de RestTemplate

---
+++ human-resources-payroll/src/main/java/com/thais/hrpayroll/entities/Worker.java
@@ -0,0 +1,18 @@
+package com.thais.hrpayroll.entities;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Worker implements Serializable {
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+}
