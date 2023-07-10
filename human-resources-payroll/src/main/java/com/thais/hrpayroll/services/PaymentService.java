63e1e3317f6c9c232f003101d869cc4b4114542c
thaisBertoldi <thais_brt@hotmail.com>
2023-07-10 18:49:50 -0300

    ref: refatoracao método de comunicacao entre servicos

--- human-resources-payroll/src/main/java/com/thais/hrpayroll/services/PaymentService.java
+++ human-resources-payroll/src/main/java/com/thais/hrpayroll/services/PaymentService.java
@@ -2,27 +2,18 @@ package com.thais.hrpayroll.services;
 
 import com.thais.hrpayroll.entities.Payment;
 import com.thais.hrpayroll.entities.Worker;
+import com.thais.hrpayroll.feignClient.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private  String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
